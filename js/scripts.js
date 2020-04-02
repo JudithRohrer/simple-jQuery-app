22a295525b361101f67143951feb40d951dbb1f9
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-04-02 18:05:27 +0200

    Starting from scratch to fix code

--- js/scripts.js
+++ js/scripts.js
@@ -13,90 +13,20 @@ var dogRepository = (function (){
     return repository;
   }
 
-  //creates a button for every dog added to the dogRepository
-  function addListItem(dog) {
-    var $button = $('<button class="pokeButton">' + dog.name + '</button>');
-    var $listItem = $('<li></li>');
-    $listItem.append($button);
-    $dogList.append($listItem);
-    //shows details when button is clicked
-    $button.on('click',function(){
-      showDetails(dog);
-    });
-  }
-
-  function showDetails(item) {
-    loadDetails(item).then(function(){
-      showModal(item);
-    });
-  }
-
-  function loadList(item) {
+  function loadList() {
     return $.ajax(apiUrl, {dataType: 'json'}).then(function (responseJSON) {
-      $.each(responseJSON.results, function(index, item) {
-        var dog = {
-          name: item.message,
-          detailsUrl: item.url
-        };
-        add(dog);
-      });
-    }).catch(function(e) {
-       console.error(e);
+      console.log(responseJSON);
+      console.log(responseJSON.message);
+      for (item in responseJSON.message){
+        console.log(item);
+      }
     });
   }
-
-  function loadDetails(item) {
-    var url = item.detailsUrl;
-    return $.ajax(url, {dataType: 'json'}).then(function (details){
-      item.imageUrl = details.sprites.front_default;
-  })
-  .catch(function(e){
-    console.error(e);
-    })
-  }
-
-  function showModal(item) {
-    $modalContainer.empty();
-
-    var $modal = $('<div class="modal"></div>')
-    var $closeButtonElement = $('<button class="modal-close">Close</button>');
-    $('.modal-close').on('click', hideModal);
-
-    var $titleELement = $('<h1> item.name </h1>');
-    var $imgElement = $('<img src="item.imageUrl">');
-
-    $modalContainer.append($titleElement);
-    $modalContainer.append($imgElement);
-
-    $modalContainer.classList.add('is-visible');
-  }
-
-  function hideModal () {
-    $modalContainer.classList.remove('is-visible');
-  }
-
-  $('window').on('keydown', (e) => {
-    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')){
-      hideModal();
-    }
-  });
-
-  $modalContainer.on('click',(e) => {
-    var target = e.target;
-    if(target === $modalContainer){
-      hideModal();
-    }
-  });
 
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem,
-    showDetails: showDetails,
-    loadList: loadList,
-    loadDetails: loadDetails,
-    showModal: showModal,
-    hideModal: hideModal
+    loadList: loadList
   };
 
 })();
