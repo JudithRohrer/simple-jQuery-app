b85c78e9ef4fc77937a924fc530714ab9bf8ec24
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-04-06 14:43:00 +0200

    syntax correction to show breedsImages

--- js/scripts.js
+++ js/scripts.js
@@ -31,20 +31,65 @@ var dogRepository = (function (){
     })
   }
 
-  function addListItem(item) {
-    var $button = $('<button class="dogButton">'+ item.name +'</button>');
+  function addListItem(dog) {
+    var $button = $('<button class="dogButton">'+ dog.name +'</button>');
     var $listItem = $('<li></li>');
     $listItem.append($button);
     $dogList.append($listItem);
-
+    $button.on('click', function(event){
+      showModal(dog);
+    });
   }
+
+
+  function showModal(dog){
+    $modalContainer.empty();
+
+    var $modal = $('<div class="modal"></div>');
+    var $closeButtonElement = $('<button class="modal-close">Close</button>');
+    $closeButtonElement.on ('click', hideModal);
+
+    var $titleElement = $('<h1>'+dog.name+'</h1>');
+
+    var dogUrl = 'https://dog.ceo/api/breed/'+dog.name+'/images/random';
+    $.ajax(dogUrl, {dataType: 'json'}).then(function (responseJSON){
+      var $breedsImage = $('<img class="modal-image" src="' + responseJSON.message + '">');
+      $modal.append($breedsImage);
+    }).catch(function(e){
+      console.error(e);
+    })
+
+    $modal.append($titleElement);
+    $modal.append($closeButtonElement);
+    $modalContainer.append($modal);
+    $modalContainer.addClass('is-visible');
+  }
+
+  function hideModal () {
+    $modalContainer.removeClass('is-visible');
+  }
+
+  $(window).on('keydown', (e) => {
+    if (e.key === 'Escape' && $modalContainer.hasClass('is-visible')){
+      hideModal();
+    }
+  });
+
+  $modalContainer.on('click',(e) => {
+    var target = e.target;
+    if(target !== $modalContainer){
+      hideModal();
+      }
+    });
 
 
   return {
     add: add,
     getAll: getAll,
     loadList: loadList,
-    addListItem: addListItem
+    addListItem: addListItem,
+    showModal: showModal,
+    hideModal: hideModal
   };
 
 })();
